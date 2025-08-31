e65db7ccf285751046ac071a1151884f3f05b8ea
juandev115 <juanhernandez125@unisangil.edu.co>
2025-08-31 18:33:30 -0500

    operaciones2

---
+++ operaciones_aritm2.py
@@ -0,0 +1,10 @@
+import math
+f = (((3*(math.pow(5,2)))+(4*(7+8))*math.pow(-2,3*2))/(math.pow(9-3,4+2*2)))+(math.sqrt(81*(2+3)))*((15-2)-7)
+print (f)
+
+g = (
+    (3 * math.pow(5, 2))
+    + (4 * (7 + 8) * math.pow(-2, 3 * 2)) / math.pow((9 - 3), (4 + 2) * 2)
+) + (math.sqrt(81) * (2 + 3)) * ((15 / 3) - 2) - 7
+
+print(g)
