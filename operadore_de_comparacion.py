b10e58df30305cf94038671ec57e5864f3278ef7
juandev115 <juanhernandez125@unisangil.edu.co>
2025-09-03 14:55:18 -0500

    operadores de comparacion

---
+++ operadore_de_comparacion.py
@@ -0,0 +1,12 @@
+#operadores de comparacion
+a = 5
+b = 10
+print(a < b) #true
+#diferencia
+print(a != b) #true
+#mayor que 
+print (a > b) #false
+#menor que 
+print (a < b ) #true
+#mayor o igual que
+print (a >= b) #false
