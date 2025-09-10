84b3bdf85698a8e71f86c68c3cf2e70057d53ba5
juandev115 <juanhernandez125@unisangil.edu.co>
2025-09-10 14:53:57 -0500

    operadores ciclicos

---
+++ loop.py
@@ -0,0 +1,19 @@
+#bucles 
+op = int(input("elija una opcion: "))
+while op != 0:
+    print("opcion incorrecta")
+print("fin del programa")
+while True:
+    numero = int(input("ingrese un numero: "))
+    if numero == 0:
+        break
+    print(f"el numero es: {numero}")
+print("fin del programa")
+"""
+while True:
+    numero = int(input("ingrese un numero: "))
+    if numero == 0:
+        break
+    print(f"el numero es: {numero}")
+print("fin del programa")
+"""
